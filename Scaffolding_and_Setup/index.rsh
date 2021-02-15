8004b5b127f0901f1879aeedf4579843507a1ade
Tahlil <bsse0803@iit.du.ac.bd>
2021-02-15 23:10:40 +0600

    scaffold completed

---
+++ Scaffolding_and_Setup/index.rsh
@@ -0,0 +1,9 @@
+'reach 0.1'
+export const main = 
+    Reach.App(
+        {},
+        [['Alice', {}], ['Bob', {}]],
+        (A, B) => {
+            exit();
+        }
+    );
---
+++ build/index.main.mjs
@@ -0,0 +1,219 @@
+// Automatically generated with Reach 0.1.2
+/* eslint-disable no-unused-vars, no-empty-pattern, no-useless-escape, no-loop-func */
+export const _version = '0.1.2';
+
+
+export async function Alice(ctc, interact) {
+  const stdlib = ctc.stdlib;
+  
+  const v0 = await ctc.creationTime();
+  
+  
+  
+  return;
+   }
+export async function Bob(ctc, interact) {
+  const stdlib = ctc.stdlib;
+  
+  const v0 = await ctc.creationTime();
+  
+  
+  
+  return;
+   }
+
+const _ALGO = {
+  appApproval: `#pragma version 2
+// Check that we're an App
+txn TypeEnum
+int appl
+==
+assert
+txn RekeyTo
+global ZeroAddress
+==
+assert
+// Check that everyone's here
+global GroupSize
+int 4
+>=
+assert
+// Check txnAppl (us)
+txn GroupIndex
+int 0
+==
+assert
+// Check txnFromHandler
+int 0
+assert
+byte base64(cw==)
+app_global_get
+gtxna 2 Args 0
+==
+assert
+byte base64(bA==)
+app_global_get
+gtxna 2 Args 4
+btoi
+==
+assert
+// Don't check anyone else, because Handler does
+// Update state
+byte base64(cw==)
+gtxna 2 Args 1
+app_global_put
+byte base64(bA==)
+global Round
+app_global_put
+byte base64(aA==)
+gtxna 2 Args 2
+btoi
+app_global_put
+byte base64(aA==)
+app_global_get
+bnz halted
+txn OnCompletion
+int NoOp
+==
+assert
+b done
+halted:
+txn OnCompletion
+int DeleteApplication
+==
+assert
+done:
+int 1
+return
+`,
+  appApproval0: `#pragma version 2
+// Check that we're an App
+txn TypeEnum
+int appl
+==
+assert
+txn RekeyTo
+global ZeroAddress
+==
+assert
+txn Sender
+byte "{{Deployer}}"
+==
+assert
+txn ApplicationID
+bz init
+global GroupSize
+int 2
+==
+assert
+txn OnCompletion
+int UpdateApplication
+==
+assert
+byte base64(cw==)
+// compute state in HM_Set
+int 0
+itob
+keccak256
+app_global_put
+byte base64(bA==)
+global Round
+app_global_put
+byte base64(aA==)
+int 0
+app_global_put
+b done
+init:
+global GroupSize
+int 1
+==
+assert
+txn OnCompletion
+int NoOp
+==
+assert
+done:
+int 1
+return
+`,
+  appClear: `#pragma version 2
+// We're alone
+global GroupSize
+int 1
+==
+assert
+// We're halted
+byte base64(aA==)
+app_global_get
+int 1
+==
+assert
+done:
+int 1
+return
+`,
+  ctc: `#pragma version 2
+// Check size
+global GroupSize
+int 4
+>=
+assert
+// Check txnAppl
+gtxn 0 TypeEnum
+int appl
+==
+assert
+gtxn 0 ApplicationID
+byte "{{ApplicationID}}"
+btoi
+==
+assert
+// Don't check anything else, because app does
+// Check us
+txn TypeEnum
+int pay
+==
+assert
+txn RekeyTo
+global ZeroAddress
+==
+assert
+txn CloseRemainderTo
+global ZeroAddress
+==
+assert
+txn GroupIndex
+int 4
+>=
+assert
+done:
+int 1
+return
+`,
+  stepargs: [0],
+  steps: [null],
+  unsupported: false
+   };
+const _ETH = {
+  ABI: `[
+  {
+    "inputs": [],
+    "stateMutability": "payable",
+    "type": "constructor"
+  },
+  {
+    "anonymous": false,
+    "inputs": [],
+    "name": "e0",
+    "type": "event"
+  }
+]`,
+  Bytecode: `0x608060408190527f49ff028a829527a47ec6839c7147b484eccf5a2a94853eddac09cef44d9d4e9e90600090a160326073565b438152603b6073565b8151815260405160519060009083906020016086565b60408051601f1981840301815291905280516020909101206000555060959050565b6040518060200160405280600081525090565b91825251602082015260400190565b603f8060a26000396000f3fe6080604052600080fdfea26469706673582212202a39c43c76a84ab32d291a946a1cce6dcd7dbf0730f57debe9210127bfedd05c64736f6c63430008000033`,
+  deployMode: `DM_constructor`
+   };
+
+export const _Connectors = {
+  ALGO: _ALGO,
+  ETH: _ETH
+   };
+
